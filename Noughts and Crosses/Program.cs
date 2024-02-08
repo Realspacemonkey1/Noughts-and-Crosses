3aed5ab6a22794f0e936b9a1f2da105fe18bed2b
Realspacemonkey1 <moonbughelper@gmail.com>
2024-02-08 16:30:34 +0000

    Added playerTurn, gameBoard, and GameBoardDisplay

--- Noughts and Crosses/Program.cs
+++ Noughts and Crosses/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool playerTurn = false; //playerTurn false == player 1 turn, playerTurn ture == player 2 turn
+string[,] gameBoard = { { "-", "-", "-" }, { "-", "-", "-" }, { "-", "-", "-" } }; //Game board is stored as an 2d array
+
+GameBoardDisplay();
+
+
+void GameBoardDisplay()
+{
+    for (int i = 0; i < gameBoard.GetLength(0); i++)
+    {
+        for (int j = 0; j < gameBoard.GetLength(1); j++)
+        {
+            Console.Write(gameBoard[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
